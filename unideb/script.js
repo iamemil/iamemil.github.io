30f79541bfba6fc046bf5ab432bc97ddb7907d74
Emil Ismayilzada <iamemil@users.noreply.github.com>
2019-09-26 20:58:25 +0200

    Add files via upload

--- unideb/script.js
+++ unideb/script.js
@@ -2,3 +2,21 @@ $('#mySelect').on('change', function (e) {
     var $optionSelected = $("option:selected", this);
     $optionSelected.tab('show')
   });
+
+
+  $('.unideb-card').click(function() {
+    clickToExapndCards($(this));
+  });
+  
+  function clickToExapndCards($obj){
+    var clickedElement = $obj;
+    if (clickedElement.hasClass('expanded')) {
+      clickedElement.find('.card-text').hide('slow');
+      clickedElement.removeClass('expanded');
+    } else {
+       clickedElement.find('.card-text').show('slow');
+      clickedElement.addClass('expanded');
+    }
+    
+  };
+  
