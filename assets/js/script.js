1ee4fe11881ecda53a8f2fa12b235490446b283c
Emil Ismayilzada <ismailzadeh.emil@gmail.com>
2021-05-07 21:44:55 +0200

    removed unnecessary files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-   // Can also be included with a regular script tag
    var typed = new Typed('.typed', {
     strings: ["ASP.NET Developer<span style='color:#00ff88'>.</span>", "Web Developer<span style='color:#00ff88'>.</span>", "C# Developer<span style='color:#00ff88'>.</span>"],
     typeSpeed: 70,
